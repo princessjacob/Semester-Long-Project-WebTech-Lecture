4d6c5dc3a94e7fe61b41f8ce5958d91880904ec8
Princess B. Jacob <2151809@slu.edu.ph>
2017-03-25 00:12:19 +0800

    Added WebtekCourseWebsite-MI only for html files these Midterms

---
+++ WebtekCourseWebsite - MI only/js/index.js
@@ -0,0 +1,11 @@
+// external js: flickity.pkgd.js
+
+/* Back to Top Script*/
+// Only enable if the document has a long scroll bar
+// Note the window height + offset
+if ( ($(window).height() + 100) < $(document).height() ) {
+    $('#top-link-block').removeClass('hidden').affix({
+        // how far to scroll down before link "slides" into view
+        offset: {top:100}
+    });
+}
